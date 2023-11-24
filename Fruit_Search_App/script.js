9d5121fc6290f5a0a661f76e69065ac2dab16577
Hasan El-Bahu <helbahu@gmail.com>
2023-11-24 03:11:19 -0500

    second commit for Fruit Search Project

--- Fruit_Search_App/script.js
+++ Fruit_Search_App/script.js
@@ -5,8 +5,8 @@ const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackb
 
 //This function accepts a string and filters for all the elements in the fruit array that contain the string, returning the filtered array.
 const search = str => {
-	let results = fruit.filter(f => f.toLowerCase().includes(str))
-	return sortArr(results,str);
+	let results = fruit.filter(f => f.toLowerCase().includes(str));
+	return results.sort((a , b)=> a.toLowerCase().indexOf(str) - b.toLowerCase().indexOf(str));
 }
 
 //This function gets the value of the input from the search bar, generates a sorted array by using the search function (above), and activates the showSuggestions function. 
@@ -31,46 +31,109 @@ const showSuggestions = (results, inputVal) =>{
 }
 
 //This function is activated once a suggestion is clicked. It will change the input value to the text of the selected input and it will clear the suggestions. 
-function useSuggestion(e) {
-	input.value = e.target.innerText;
+function useSuggestion() {
+	input.value = document.querySelector(`.selected`).innerText;
 	suggestions.innerHTML = ``;
 }
 
 
 //Additional Functions
 
-//This function sorts the array to be in order of relevance (the earlier the string occurs in the element) to the typed string. 
-const sortArr = (arr,str) => {
-	let array = [];
-	let n = 0;
-	while (array.length !== arr.length){
-		arr.forEach(fruit =>{
-			if(fruit.toLowerCase().indexOf(str) === n){
-				array.push(fruit);
-			}
-		})
-		n++;
-	}
-	return array;
-}
-
 //This function highlights the item that is hovered over when the mouse is over.
 const highlightSuggestion = e => {
-	e.target.style.backgroundColor = `hsl(${n2*0.9}, 100%, 35%)`;
-	e.target.style.color = `white`;
+	if(document.querySelector(`.selected`))highlight(document.querySelector(`.selected`),`remove`);
+	highlight(e.target);
 	suggestions.addEventListener('mouseout', e =>{
-		e.target.style.backgroundColor = ``;		
-		e.target.style.color = ``;
+		highlight(e.target,`remove`);
 	});
 }
+const highlight = (first,second) => {
+	if(second === undefined){
+		addHighlight(first);
+	}else if(second === `remove`){
+		removeHighlight(first);
+	}else{
+		removeHighlight(first);
+		addHighlight(second);
+	}
+}
+const addHighlight = li => {
+	li.classList.add(`selected`);
+	li.style.backgroundColor = `hsl(${n2*0.9}, 100%, 35%)`;
+	li.style.border = `white solid 0.2vh`;
+	li.style.color = `white`;
+}
+const removeHighlight = li => {
+	li.classList.remove(`selected`);
+	li.style.backgroundColor = ``;
+	li.style.border = ``;
+	li.style.color = ``;
+}
 
+//Scroll function
+const scroll = (direction,arr,idx) => {
+	let scroll = document.querySelector(`.suggestions`);
+	let liHt = (arr[0].clientHeight)*(idx - 4);
+
+	idx < (scroll.scrollTop/(arr[0].clientHeight))?scroll.scrollTo(0,(arr[0].clientHeight)*(idx)):null;
+	idx > (scroll.scrollTop/(arr[0].clientHeight) + 5)?scroll.scrollTo(0,liHt):null;
+
+
+	if((direction === `down`)&&(idx >=5)){
+		(scroll.scrollTop/(arr[0].clientHeight) + 5) === idx ?scroll.scrollTo(0,liHt):null;
+		(idx === arr.length-1)?scroll.scrollTo(0,0):null;
+						
+	}
+	if(direction === `up`){
+		(scroll.scrollTop/(arr[0].clientHeight)) === idx ? scroll.scrollTo(0,(arr[0].clientHeight)*(idx-1)):null;
+		(idx === 0)? scroll.scrollTo(0,(arr[0].clientHeight)*(arr.length - 6)):null;
+	}
+
+
+}
 
 
 //Event Listeners 
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('mousedown', useSuggestion);
+input.addEventListener('keyup',(e)=>{
+	if((e.key !== `ArrowUp`)&&(e.key !== `ArrowDown`)&&(e.key !== `Enter`)){
+		searchHandler(e);
+	}
+});
+suggestions.addEventListener('mousedown', (e => useSuggestion()));
 suggestions.addEventListener('mouseover', highlightSuggestion);
+input.addEventListener(`keydown`,(e)=>{
+	if((e.key === `ArrowUp`)||(e.key === `ArrowDown`)){
+		let liList = [...document.querySelectorAll(`.suggestions ul li`)]; 
+		if((e.key === `ArrowDown`)&&(liList.length > 0)){
+			if(liList.every(li => !li.classList.contains(`selected`))){
+				highlight(liList[0]);
+			}else{
+				let idx = liList.findIndex(li => li.classList.contains(`selected`));
+				(idx < liList.length - 1) ? highlight(liList[idx],liList[idx+1]):highlight(liList[idx],liList[0]);
 
+				scroll(`down`,liList,idx);
+				
+
+			}
+		}else if((e.key === `ArrowUp`)&&(liList.length > 0)){
+			if(liList.every(li => !li.classList.contains(`selected`))){
+				highlight(liList[liList.length-1]);
+				scroll(`down`,liList,liList.length-2);
+
+			}else{
+				let idx = liList.findIndex(li => li.classList.contains(`selected`));
+				(idx > 0) ? highlight(liList[idx],liList[idx-1]):highlight(liList[idx],liList[liList.length-1]);
+				scroll(`up`,liList,idx);
+
+			}
+		}	
+	}
+
+	if(e.key === `Enter`){
+		useSuggestion();
+	}
+
+})
 
 
 
@@ -91,6 +154,6 @@ setInterval(()=>{
 		n2 -=3;	
 	}
 
-},400)
+},500)
 
 
--- Fruit_Search_App/script.test.js
+++ Fruit_Search_App/script.test.js
@@ -5,10 +5,6 @@ describe(`Friut Search Core Functions`,()=>{
         expect(search(`lb`)).toEqual([`Bilberry`,`Mulberry`]);
     });
 
-    it(`The sortArr function accepts an array and a string and sorts the array based on where the string occurs in the array`,()=>{
-        expect(sortArr([`123x5`,`1x345`,`1234x`,`x2345`,`12x45`],`x`)).toEqual([`x2345`,`1x345`,`12x45`,`123x5`,`1234x`]);
-    });
-
     it(`The showSuggestions function accepts an array and a string, and appends each list element to the suggestions dropdown with the inputVal bold`,()=>{
         showSuggestions([`Test text1`,`Test text2`],`text`);
         let suggestionsList = [...document.querySelectorAll(`.suggestions ul li`)];
