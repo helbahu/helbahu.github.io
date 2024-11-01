01b8f30642359777f442b47c6870c60826796f35
Hasan El-Bahu <helbahu@gmail.com>
2024-11-01 03:41:15 -0400

    Fixed configuration of fcm application

---
+++ Test_FCM_App/frontend/public/firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+// Here I am using Firebase version 10.12.3, 
+// you can use Firebase version 10.12.3 and above, 
+// because the Firebase legacy API has been turned off as of July 20 2024,
+// see https://firebase.google.com/docs/cloud- messaging/migrate-v1
+
+importScripts("swenv.js");
+importScripts("https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.3/firebase-app-compat.min.js");
+importScripts("https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.3/firebase-messaging-compat.min.js");
+
+console.log("SWENV   !!!   ",process.env);
+
+firebase.initializeApp(process.env);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(async (message) => {
+  console.log("firebase-messaging-sw.js: Received background message ", message);
+
+  self?.registration?.showNotification(message?.data?.title || "Notification Title", {
+    icon: message?.data?.icon,
+    badge: message?.data?.badge,
+    body: message?.data?.body
+  })
+})
---
+++ Test_FCM_App/frontend/swEnvBuild.js
@@ -0,0 +1,21 @@
+//swEnvBuild.js        
+require('dotenv').config();     
+const fs = require('fs');
+
+console.log("ENV VARZ = ",process.env.REACT_APP_MEASUREMENT_ID);
+
+fs.writeFileSync(
+  './public/swenv.js',
+  `
+ const process = {
+    env: {
+        "apiKey": '${process.env.REACT_APP_API_KEY}',
+        "authDomain": '${process.env.REACT_APP_AUTH_DOMAIN}',
+        "projectId": '${process.env.REACT_APP_PROJECT_ID}',
+        "storageBucket": '${process.env.REACT_APP_STORAGE_BUCKET}',
+        "messagingSenderId": '${process.env.REACT_APP_MESSAGING_SENDER_ID}',
+        "appId": '${process.env.REACT_APP_APP_ID}',
+        "measurementId": '${process.env.REACT_APP_MEASUREMENT_ID}'
+        }
+ }`
+);
