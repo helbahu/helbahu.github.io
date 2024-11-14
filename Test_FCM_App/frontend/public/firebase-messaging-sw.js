489dcaaad7a8417ccabecb6ff706b2ef1600cf7e
Hasan El-Bahu <helbahu@gmail.com>
2024-11-14 17:32:30 -0500

    updated FCM service worker

--- Test_FCM_App/frontend/public/firebase-messaging-sw.js
+++ Test_FCM_App/frontend/public/firebase-messaging-sw.js
@@ -3,13 +3,18 @@
 // because the Firebase legacy API has been turned off as of July 20 2024,
 // see https://firebase.google.com/docs/cloud- messaging/migrate-v1
 
-importScripts("swenv.js");
 importScripts("https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.3/firebase-app-compat.min.js");
 importScripts("https://cdnjs.cloudflare.com/ajax/libs/firebase/10.12.3/firebase-messaging-compat.min.js");
 
-console.log("SWENV   !!!   ",process.env);
-
-firebase.initializeApp(process.env);
+firebase.initializeApp({
+  "apiKey": 'AIzaSyCaU32s0LFWhtJES2IvltZ1LvC4AEcHDqc',
+  "authDomain": 'fir-test-project1-29b77.firebaseapp.com',
+  "projectId": 'fir-test-project1-29b77',
+  "storageBucket": 'fir-test-project1-29b77.appspot.com',
+  "messagingSenderId": '1075656025401',
+  "appId": '1:1075656025401:web:05c434f90f617e7fba34e8',
+  "measurementId": 'G-EGXWEGE1CV'
+});
 
 const messaging = firebase.messaging();
 
