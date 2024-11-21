ae6862f6b559d8cb2d9c8288a60825b4d58597a0
Hasan El-Bahu <helbahu@gmail.com>
2024-11-20 20:46:24 -0500

    Updated send-messages to FCM routes

--- Test_FCM_App/backend/fcmRoutes.js
+++ Test_FCM_App/backend/fcmRoutes.js
@@ -7,14 +7,16 @@ import { messaging } from './fcmApp.js';
 router.post("/subscribe-to-topic", async (req, res) => {
   try {
     const token = req.body.token || req.query.token || req.headers["x-fcm-token"];
-
+    const genre = req.body.genre || all;
     console.log("TOKEN IS",token);
+    console.log("GENRE IS",genre);
 
     // Subscribe to the "all" topic
-    await messaging.subscribeToTopic(token,"all").then(() => {
+    await messaging.subscribeToTopic(token,genre).then(() => {
       console.log("Subscribed To Topic Successfully");
       res.status(200).json({
-        message: "Subscribed To Topic Successfully"
+        message: "Subscribed To Topic Successfully",
+        topic:genre
       });
     });
 
@@ -63,6 +65,54 @@ router.post("/send-message", async (req, res) => {
 });
 
 
+router.post("/send-messages", async (req, res) => {
+  try {
+    const messages = JSON.parse(req.body.messages);
+    console.log("MESSAGES LIST   :   ",messages);
+    if(messages?.length){
+      const sentMessages = [];
+      for(const msg of messages){
+        const {title,body,icon,genre} = msg;
+  
+        const message = {
+          // token, // token is required
+          topic: genre || "all",
+          data: {
+            ...msg,
+            title: title || "AfroWatch Notification",
+            body: body,
+            icon: icon || "https://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_28dp.png"
+    
+          }
+        };
+
+        // Send the message
+        sentMessages.push(messaging.send(message));
+
+      }
+  
+      await Promise.all(sentMessages).then(()=>{
+        res.status(200).json({
+          message: "All Notifications sent successfully",
+        });
+      });
+    
+    }else{
+      res.status(200).json({
+        message: "No messages were in the given list."
+      });
+  
+    }
+
+  } catch (error) {
+    res.status(error?.status || 500).json({
+      status: "error",
+      message: error?.message || "Something went wrong",
+      error
+
+    });
+  }
+});
 
 
 export default router;
